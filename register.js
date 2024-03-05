df6e97ec977a2095ffead6ded30b6070c3254092
K-levr <rigdaokyle@gmail.com>
2024-03-05 15:33:20 -0800

    register-web

---
+++ register.js
@@ -0,0 +1,28 @@
+console.log("Hello World!");
+console.error("ERROR HAPPENED!!");
+console.warn("Warning!!!!!")
+
+//variables
+//string = "Words"
+//number = 123, 123e, 234.76
+//boolean = true, false
+//object = ["apple", "banana"], ["1, 2, 3"]
+           //["apple, 3, true"]
+//type of
+//console.log(typrof(["apple", "banana"]))
+
+//declaration of variables
+let name = "Kyle",
+ lastname = `Rigdao`,
+ age = 19,
+ isProf = true,
+ hobby = ["Playing softball"]
+ run = function () {
+    console.log("Im running.");
+ }; //function
+console.log(name, age, isProf, hobby);
+console.log(typeof run);
+console.table(hobby);
+console.log("My name is "+ name + " " + lastname);
+console.log(`My name is ${name} ${lastname}`);
+//folder / Repo name = register-web
